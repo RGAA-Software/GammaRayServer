942d95d3fa4d52e56576fe8244d66426dcf4f207
RGAA-Software <hollow.ichigo.yoo@gmail.com>
2025-03-19 14:21:17 +0800

    debug
    Signed-off-by: RGAA-Software <hollow.ichigo.yoo@gmail.com>

--- supervisor/src/main.rs
+++ supervisor/src/main.rs
@@ -11,6 +11,7 @@ mod spvr_grpc_client_mgr_trait;
 mod spvr_handler_server;
 mod spvr_defs;
 mod spvr_handler_device;
+mod spvr_event_mgr;
 
 use crate::spvr_conn_mgr::SpvrConnManager;
 use crate::spvr_grpc_profile_client_mgr::SpvrGrpcProfileClientMgr;
@@ -20,12 +21,14 @@ use crate::spvr_settings::SpvrSettings;
 use base::log_util;
 use std::sync::Arc;
 use tokio::sync::Mutex;
+use crate::spvr_event_mgr::SpvrEventManager;
 
 lazy_static::lazy_static! {
     pub static ref gSpvrSettings: Arc<Mutex<SpvrSettings>> = Arc::new(Mutex::new(SpvrSettings::new()));
     pub static ref gSpvrConnMgr: Arc<Mutex<SpvrConnManager>> = Arc::new(Mutex::new(SpvrConnManager::new()));
     pub static ref gSpvrGrpcRelayClientMgr: Arc<Mutex<SpvrGrpcRelayClientManager >> = Arc::new(Mutex::new(SpvrGrpcRelayClientManager::new()));
     pub static ref gSpvrGrpcProfileClientMgr: Arc<Mutex<SpvrGrpcProfileClientMgr >> = Arc::new(Mutex::new(SpvrGrpcProfileClientMgr::new()));
+    pub static ref gSpvrEventManager: Arc<Mutex<SpvrEventManager>> = Arc::new(Mutex::new(SpvrEventManager::new()));
 }
 
 #[tokio::main]
@@ -35,7 +38,10 @@ async fn main() {
 
     // settings
     gSpvrSettings.lock().await.load();
-
+    
+    // event manager
+    gSpvrEventManager.lock().await.init().await;
+    
     // context
     let context = Arc::new(Mutex::new(spvr_context::SpvrContext::new()));
     
